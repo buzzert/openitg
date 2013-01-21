8c36dc1685ec34f0a47e491edba0a7f812748fb7
James Magahern <james@magahern.com>
2013-01-20 19:44:29 -0800

    Added support for the MiniMaid USB Adapter.
    TODO: Pad light mappings.

---
+++ src/io/MiniMaid.h
@@ -0,0 +1,18 @@
+#ifndef IO_MINIMAID_H
+#define IO_MINIMAID_H
+
+#include <stdint.h>
+#include "USBDriver.h"
+
+class MiniMaid: public USBDriver
+{
+public:
+	bool Open();
+	
+	bool Read( uint64_t *pData );
+	bool Write( uint64_t data );
+	
+	bool m_bInitialized;
+};
+
+#endif /* IO_MINIMAID_H */
